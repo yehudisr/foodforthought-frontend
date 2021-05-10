a7ad29a69fced23b1f321428546dd92957206eb6
yehudisr <77118499+yehudisr@users.noreply.github.com>
2021-05-10 15:17:28 -0400

    working on location pins with geocode api

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -1,33 +1,53 @@
 import React from "react"
-import { useState, useRef, useCallback } from 'react'
+import { useState, useRef, useCallback, useEffect } from 'react'
 import { GoogleMap, useLoadScript, LoadScript, Marker, InfoWindow, useJsApiLoader } from '@react-google-maps/api'
 import mapStyles from './mapStyles'
+import {
+  geocodeByAddress,
+  geocodeByPlaceId,
+  getLatLng,
+} from 'react-places-autocomplete'
+import Geocode from "react-geocode"
+import {useSelector} from 'react-redux'
 
 
-const libraries = ["places"];
+const libraries = ["places"]
 
 const mapContainerStyle = {
   height: "45vh",
   width: "90vw",
-};
+}
 const options = {
   styles: mapStyles,
   disableDefaultUI: true,
   zoomControl: true,
-};
+}
 const center = {
   lat: 40.7128,
   lng: -73.935242,
-};
+}
 
 const MapContainer = () => {
-
-    const [selected, setSelected] = useState(false)
+const foodOrders = useSelector(state => state.foodOrder)
+const [selected, setSelected] = useState(false)
+const [ currentPosition, setCurrentPosition ] = useState({});
 
 const { isLoaded } = useJsApiLoader({
         id: 'google-map-script',
         googleMapsApiKey: process.env.REACT_APP_GOOGLE_API_KEY
       })
+
+  const success = position => {
+    const currentPosition = {
+      lat: position.coords.latitude,
+      lng: position.coords.longitude
+    }
+    setCurrentPosition(currentPosition);
+  };
+  
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(success);
+  })
 
   const [map, setMap] = React.useState(null)
       const onLoad = React.useCallback(function callback(map) {
@@ -39,40 +59,53 @@ const { isLoaded } = useJsApiLoader({
         setMap(null)
       }, [])
 
-  if (!isLoaded) return "Loading...";
+  if (!isLoaded) return "Loading..."
 
-//  const success = position => {
-//     const currentPosition = {
-//       lat: position.coords.latitude,
-//       lng: position.coords.longitude
-//     }
-//     setCurrentPosition(currentPosition);
-//   }
+  
   
   const onSelect = item => {
     setSelected(item);
   }
-  
-  
-//   const defaultCenter = {
-//     lat: 40.7128, lng: 74.0060
-//   }
 
-  const locations = [
-    {
-      name: "Location 1",
-      location: { 
-        lat: 40.6703,
-        lng: -73.9423 
-      },
-    },
-    {
-      name: "Location 2",
-      location: { 
-        lat: 40.6702,
-        lng: -73.9363
-      },
-    },
+Geocode.setApiKey(process.env.REACT_APP_GOOGLE_API_KEY)
+
+const locations = foodOrders.map(order => order.food_giver.location)
+
+function locationDisplay(locations) { 
+    locations.map(
+    location => 
+    { console.log(location)
+        Geocode.fromAddress(location).then(
+        (response) => { 
+            console.log(response)
+        const { lat, lng } = response.results[0].geometry.location;
+        console.log(lat, lng);
+           },
+            (error) => {
+          console.error(error);
+        }
+       )
+    }
+//  return { lat, lng }
+)
+}
+//   locationDisplay(locations)
+  
+//   const locations = [
+//     {
+//       name: "Location 1",
+//       location: { 
+//         lat: 40.6703,
+//         lng: -73.9423 
+//       },
+//     },
+//     {
+//       name: "Location 2",
+//       location: { 
+//         lat: 40.6702,
+//         lng: -73.9363
+//       },
+//     },
     // {
     //   name: "Location 3",
     //   location: { 
@@ -94,15 +127,15 @@ const { isLoaded } = useJsApiLoader({
     //     lng: 2.1915
     //   },
     // }
-  ];
+//   ];
   
 
   return (
 
         <GoogleMap
           mapContainerStyle={mapContainerStyle}
-          zoom={6}
-          center={center}
+          zoom={11}
+          center={currentPosition}
           options={options}
           onLoad={onLoad}
           onUnmount={onUnmount}
